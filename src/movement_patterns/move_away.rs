f900ce28ea424f8b6a4bc104020485336f4d627a
Max Carvajal <jcarvajal288@gmail.com>
2025-03-11 20:45:51 -0500

    Make MoveAway work for accelerating or deceleratng

--- src/movement_patterns/move_away.rs
+++ src/movement_patterns/move_away.rs
@@ -9,6 +9,7 @@ pub struct MoveAway {
     pub final_velocity: f32,
     pub acceleration: f32,
     pub direction: Vec3,
+    pub is_accelerating: bool,
 }
 
 impl MovementPattern for MoveAway {
@@ -17,7 +18,8 @@ impl MovementPattern for MoveAway {
             self.direction = (transform.translation - self.repulsion_point).normalize().with_z(0.0);
         }
         let delta_time = time.delta_secs();
-        if self.velocity > self.final_velocity {
+        if self.is_accelerating && self.velocity < self.final_velocity
+        || !self.is_accelerating && self.velocity > self.final_velocity {
             self.velocity += self.acceleration * delta_time;
         }
         transform.translation += self.direction * self.velocity * delta_time;
@@ -36,7 +38,7 @@ pub struct MoveAwayBuilder {
     pub repulsion_point: Vec3,
     pub starting_velocity: f32,
     pub final_velocity: f32,
-    pub time_to_decelerate: Duration,
+    pub time_to_final_velocity: Duration,
 }
 
 pub fn build_move_away(builder: MoveAwayBuilder) -> MoveAway {
@@ -44,8 +46,9 @@ pub fn build_move_away(builder: MoveAwayBuilder) -> MoveAway {
         repulsion_point: builder.repulsion_point,
         velocity: builder.starting_velocity,
         final_velocity: builder.final_velocity,
-        acceleration: (builder.final_velocity - builder.starting_velocity) / builder.time_to_decelerate.as_secs_f32(),
+        acceleration: (builder.final_velocity - builder.starting_velocity) / builder.time_to_final_velocity.as_secs_f32(),
         direction: Vec3::ZERO,
+        is_accelerating: builder.starting_velocity < builder.final_velocity,
     }
 }
 
